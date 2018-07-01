35af7d2514f73bd76e718cdde7f875ad4f2af7f5
Émerson Pinto Homrich <ehomrich@users.noreply.github.com>
2018-07-01 05:44:20 -0300

    Add endpoint to the API for recipes search
    Adds a /recipes/ endpoint to search through the Recipe Puppy API.
    Expects a "i" parameter with 1 up to 3 ingredients. Manipulates the
    response in order to met the required format. Logic is still very
    procedural and will be refactored.

--- app.js
+++ app.js
@@ -13,18 +13,65 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/recipes/', (req, res) => {
+  if (req.query === {} || !req.query.i) {
+    res.status(400).send({
+      error: 'You must provide at least one ingredient'
+    });
+  } else if (req.query.i.split(/,\s*/).length > 3) {
+    res.status(400).send({
+      error: 'Too many ingredients. You must provide 1 to 3 ingredients'
+    });
+  } else {
+    getRecipes(req.query.i)
+      .then(recipes => {
+        res.json({
+          keywords: req.query.i.split(/,\s*/),
+          recipes: handleRecipes(recipes.data.results)
+        });
+      })
+      .catch(error => {
+        if (error.response.status === 500) {
+          res.json({
+            error: 'Request to Recipe Puppy API failed with status code 500'
+          });
+        }
+      });
+  }
+});
+
 app.get('/giphy/', (req, res) => {
   getGif(req.query.q)
     .then(results => {
       res.send(results.data.data[0].images.original.url);
     })
     .catch(error => {
-      res.send({
-        status: error.response.status,
-        message: error.message
-      });
+      res.send(error.response);
     });
 });
+
+function getRecipes(ingredients) {
+  return axios.get(RECIPEPUPPY_API, {
+    params: {
+      i: ingredients
+    }
+  });
+}
+
+function handleRecipes(recipes) {
+  const processedRecipes = [];
+
+  for (let recipe of recipes) {
+    processedRecipes.push({
+      title: recipe.title.trim(),
+      ingredients: recipe.ingredients.split(', ').sort(),
+      link: recipe.href,
+      gif: null
+    });
+  }
+
+  return processedRecipes;
+}
 
 function getGif(query) {
   return axios.get(GIPHY_API, {
